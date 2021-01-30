55039b81fbfaf2001b75bde49930f4cce7c88235
YaDarth2709 <77859646+YaDarth2709@users.noreply.github.com>
2021-01-31 00:06:57 +0300

    Delete SakiCore directory

--- SakiCore/src/com/edasaki/core/utils/gson/GsonBuilderModifiers.java
+++
@@ -1,8 +0,0 @@
-package com.edasaki.core.utils.gson;
-
-import com.google.gson.GsonBuilder;
-
-@FunctionalInterface
-public interface GsonBuilderModifiers {
-    public GsonBuilder modify(GsonBuilder builder);
-}
